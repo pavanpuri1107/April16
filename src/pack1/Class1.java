2c95a81d32f27323153107c43dd10e1dc7aeaeea
pavanpuri <99785740+pavanpuri@users.noreply.github.com>
2022-08-06 20:28:58 +0530

    commit1

--- src/pack1/Class1.java
+++ src/pack1/Class1.java
@@ -5,7 +5,8 @@ public class Class1
 	public static void main(String[] args) 
 	{
 		System.out.println("Hii-this is first line");
-		
+		System.out.println("Hii this is second line");
+	    System.out.println("RAHUL");
 	}
 
 }
